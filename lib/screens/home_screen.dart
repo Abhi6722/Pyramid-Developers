a07b01c4cc5e4da8366cff436c4e2ba87afd440f
Abhi6722 <abhinav6722cool@gmail.com>
2024-01-09 16:53:35 +0530

    Fast Warmup Section Added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pyramid_developers/widgets/best_for_you.dart';
 import 'package:pyramid_developers/widgets/challenge.dart';
+import 'package:pyramid_developers/widgets/fast_warmup.dart';
 import 'package:pyramid_developers/widgets/home_screen_banner.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -12,15 +13,19 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: Color(0XFFF7F6FA),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 18.0),
-        child: Column(
-          children: [
-            SizedBox(height: 100,),
-            HomeScreenBanner(),
-            SizedBox(height: 24,),
-            BestForYou(),
-            SizedBox(height: 30,),
-            Challenges(),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 100,),
+              HomeScreenBanner(),
+              SizedBox(height: 24,),
+              BestForYou(),
+              SizedBox(height: 30,),
+              Challenges(),
+              SizedBox(height: 30,),
+              FastWarmup(),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/fast_warmup.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:pyramid_developers/widgets/exercise_card.dart';
+
+class FastWarmup extends StatelessWidget {
+  const FastWarmup({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Fast Warmup',
+          style: TextStyle(
+            color: Color(0xFF192126),
+            fontSize: 18,
+            fontFamily: 'Lato',
+            fontWeight: FontWeight.w700,
+            height: 0,
+          ),
+        ),
+        SizedBox(height: 21,),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  ExerciseCard(
+                      title: "Leg excercises",
+                      imageUrl: "assets/images/leg_excercises.png",
+                      duration: "10 min",
+                      difficulty: "Beginner"),
+                  SizedBox(width: 16,),
+                  ExerciseCard(
+                      title: "Backward lunges",
+                      imageUrl: "assets/images/backward_lunges.png",
+                      duration: "5 min",
+                      difficulty: "Beginner"),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
